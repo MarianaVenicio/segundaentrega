2187731085a338eba987feefcbc1fce6e496ca14
mariana venicio <marianaandreavenicio@hotmail.com>
2022-12-13 21:36:21 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,158 @@
+// Variables
+const baseDeDatos = [
+    {
+        id: 1,
+        nombre: 'serum',
+        precio: 100,
+        imagen: 'serum-facial'
+    },
+    {
+        id: 2,
+        nombre: 'balsamo',
+        precio: 1200,
+        imagen: 'balsamo-labial.jpg'
+    },
+    {
+        id: 3,
+        nombre: 'jabon',
+        precio: 210,
+        imagen: 'jabon-facial.jpg'
+    },
+    {
+        id: 4,
+        nombre: 'mascarilla',
+        precio: 600,
+        imagen: 'mascarilla-facial.jpg'
+    }
+
+];
+
+let carrito = [];
+const divisa = '$';
+const DOMitems = document.querySelector('#items');
+const DOMcarrito = document.querySelector('#carrito');
+const DOMtotal = document.querySelector('#total');
+const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+
+function renderizarProductos() {
+    baseDeDatos.forEach((info) => {
+        
+        const miNodo = document.createElement('div');
+        miNodo.classList.add('card', 'col-sm-4');
+        
+        const miNodoCardBody = document.createElement('div');
+        miNodoCardBody.classList.add('card-body');
+        
+        const miNodoTitle = document.createElement('h5');
+        miNodoTitle.classList.add('card-title');
+        miNodoTitle.textContent = info.nombre;
+        
+        const miNodoImagen = document.createElement('img');
+        miNodoImagen.classList.add('img-fluid');
+        miNodoImagen.setAttribute('src', info.imagen);
+        // Precio
+        const miNodoPrecio = document.createElement('p');
+        miNodoPrecio.classList.add('card-text');
+        miNodoPrecio.textContent = `${info.precio}${divisa}`;
+        // Boton 
+        const miNodoBoton = document.createElement('button');
+        miNodoBoton.classList.add('btn', 'btn-primary');
+        miNodoBoton.textContent = '+';
+        miNodoBoton.setAttribute('marcador', info.id);
+        miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+        
+        miNodoCardBody.appendChild(miNodoImagen);
+        miNodoCardBody.appendChild(miNodoTitle);
+        miNodoCardBody.appendChild(miNodoPrecio);
+        miNodoCardBody.appendChild(miNodoBoton);
+        miNodo.appendChild(miNodoCardBody);
+        DOMitems.appendChild(miNodo);
+    });
+}
+
+
+function anyadirProductoAlCarrito(evento) {
+   
+    carrito.push(evento.target.getAttribute('marcador'))
+    
+    renderizarCarrito();
+
+}
+
+
+function renderizarCarrito() {
+   
+    DOMcarrito.textContent = '';
+    
+    const carritoSinDuplicados = [...new Set(carrito)];
+  
+    carritoSinDuplicados.forEach((item) => {
+        
+        const miItem = baseDeDatos.filter((itemBaseDatos) => {
+           
+            return itemBaseDatos.id === parseInt(item);
+        });
+       
+        const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+            
+            return itemId === item ? total += 1 : total;
+        }, 0);
+        
+        const miNodo = document.createElement('li');
+        miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+        miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
+       
+        const miBoton = document.createElement('button');
+        miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+        miBoton.textContent = 'X';
+        miBoton.style.marginLeft = '1rem';
+        miBoton.dataset.item = item;
+        miBoton.addEventListener('click', borrarItemCarrito);
+        
+        miNodo.appendChild(miBoton);
+        DOMcarrito.appendChild(miNodo);
+    });
+    
+    DOMtotal.textContent = calcularTotal();
+}
+
+
+function borrarItemCarrito(evento) {
+   
+    const id = evento.target.dataset.item;
+  
+    carrito = carrito.filter((carritoId) => {
+        return carritoId !== id;
+    });
+    
+    renderizarCarrito();
+}
+
+
+function calcularTotal() {
+    
+    return carrito.reduce((total, item) => {
+        
+        const miItem = baseDeDatos.filter((itemBaseDatos) => {
+            return itemBaseDatos.id === parseInt(item);
+        });
+        
+        return total + miItem[0].precio;
+    }, 0).toFixed(2);
+}
+
+
+function vaciarCarrito() {
+   
+    carrito = [];
+    
+    renderizarCarrito();
+}
+
+
+DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+
+
+renderizarProductos();
+renderizarCarrito();
+
